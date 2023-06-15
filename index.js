7c76a85ff38d89759d53482446cd341e5b085f13
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2023-06-15 13:54:49 +0700

    setting vercel

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.json({ message: "ini adalah data" });
-// });
-
 require("./src/routers/produk.routers")(app);
 
 const PORT = 8100;
